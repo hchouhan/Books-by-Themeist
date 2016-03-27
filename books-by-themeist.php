0104317385d3e9e4754275924b31cfacf1f8f4ec
Harish Chouhan <hello@dreamsmedia.in>
2016-03-28 01:31:37 +0530

    Add textdomain support

--- books-by-themeist.php
+++ books-by-themeist.php
@@ -2,6 +2,8 @@
 /**
  * Plugin Name: Books by Themeist
  * Plugin URI: http://themeist.co
+ * Text Domain: books-by-themeist
+ * Domain Path: /languages
  * Description: Books CPT for theme developers to use in their client projects
  * Version: 0.1
  * Author: Harish Chouhan
@@ -18,7 +20,14 @@ class Themeist_Books {
 		add_action( 'plugins_loaded', array( &$this, 'constants' ), 1 );
 		add_action( 'plugins_loaded', array( &$this, 'includes' ), 2 );
 		add_action( 'plugins_loaded', array( &$this, 'admin' ), 3 );
+		add_action( 'plugins_loaded', array( &$this, 'my_plugin_load_plugin_textdomain' ), 4 );
 	}
+
+	function my_plugin_load_plugin_textdomain() {
+	    load_plugin_textdomain( 'books-by-themeist', FALSE, basename( dirname( __FILE__ ) ) . '/languages/' );
+	}
+	
+
 
 	/**
 	 * Defines constants for the plugin.
